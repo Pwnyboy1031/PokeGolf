6a7427471f1db5b40b5424c75ac97fa615ee3795
Taylor Todd <ttodd1031@gmail.com.com>
2023-03-28 14:20:03 -0500

    moved script to body / split dom logic and init

--- src/index.js
+++ src/index.js
@@ -1,33 +1,23 @@
-console.log("hello poke golf!")
-
 import Pokedex from 'pokedex-promise-v2';
+import { homeLoad } from './homeLoad';
 const P = new Pokedex();
 const targetPokemonId = Math.floor(Math.random() * 151) + 1;
 const userGuess = 1;
 
-
 async function getPokemonSprite() {
-        try {
-            const pokemon = await P.getResource([`api/v2/pokemon/${targetPokemonId}`]);
-            const pokemonSpriteURL = pokemon[0].sprites.front_default;
-            return pokemonSpriteURL;
-        } catch (error) {
-            throw error
-        }
+    try {
+        const pokemon = await P.getResource([`api/v2/pokemon/${targetPokemonId}`]);
+        const pokemonSpriteURL = pokemon[0].sprites.front_default;
+        return pokemonSpriteURL;
+    } catch (error) {
+        throw error
     }
+}
 
-
-document.addEventListener("DOMContentLoaded", async (e) => {
-    //dom stuff
-    const app = document.getElementById("app");
-    const spriteContainer = document.createElement("img");
-    spriteContainer.setAttribute("id", "pokemonSprite");
-    app.appendChild(spriteContainer);
+async function init() {
     const pokemonSpriteURL = await getPokemonSprite();
-    spriteContainer.setAttribute("src", pokemonSpriteURL);
-    const userInputBox = document.createElement("input");
-    userInputBox.setAttribute("maxlength", 3)
-    app.appendChild(userInputBox);
-});
+    homeLoad(pokemonSpriteURL);
+}
 
+init();
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,13 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: "development",
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  experiments: {
+    topLevelAwait: true
+  },
+  
 };
